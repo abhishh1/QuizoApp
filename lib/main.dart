5a62db81d183c45148a4adbfa9a1207cb6530244
Abhishek Chavhan <52340362+abhishh1@users.noreply.github.com>
2020-04-24 14:45:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,12 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
